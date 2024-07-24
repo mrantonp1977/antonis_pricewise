be6c23c9a01669334dc8d9a8fd53668ae069c53c
Antonis Papaioannou <mrantonp@gmail.com>
2024-07-24 20:30:21 +0300

    make search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+]
 
 const Navbar = () => {
   return (
     <header className="w-full">
-      <nav>
-
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image 
+            src="/assets/icons/logo.svg"
+            alt='logo'
+            width={32}
+            height={32}
+          />
+          <p className="nav-logo">
+            Price<span className="text-primary">Wise</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt} 
+              src={icon.src}
+              alt={icon.alt}
+              width={30}
+              height={30}
+              className="object-contain"
+            />
+          ))}
+        </div>
       </nav>
     </header>
   )
